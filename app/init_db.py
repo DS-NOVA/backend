264373f06ef68c578851c5fafecb742c55f0342c
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-05-20 18:26:12 +0900

    [feat] user 모델 생성 #1

--- app/init_db.py
+++ app/init_db.py
@@ -1,12 +1,13 @@
 # 데이터베이스 테이블 생성 시 필요
 
 from app.db.database import Base, engine
-from app.models.test import Test #이 부분에 import 
+#from app.models.test import Test #이 부분에 import 
+from app.models.user import User
 
 def init():
-    print("[test] nova 데이터베이스 생성중 ...")
+    print("nova 데이터베이스 생성중 ...")
     Base.metadata.create_all(bind=engine) #새로 import 된 테이블만 생성
-    print("[test] nova 데이터베이스 생성 완료")
+    print("nova 데이터베이스 생성 완료")
 
 if __name__ == "__main__":
     init()
