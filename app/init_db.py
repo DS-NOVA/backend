5cea4dda80a86d9b945b923dadc04f4393e4fc03
JungminPark <wjdals9171@naver.com>
2025-05-23 12:34:35 +0900

    [chore] init_db.py 테이블 생성 스크립트 정리 #3

--- app/init_db.py
+++ app/init_db.py
@@ -1,12 +1,9 @@
-# 데이터베이스 테이블 생성 시 필요
-
 from app.db.database import Base, engine
-#from app.models.test import Test #이 부분에 import 
-from app.models.user import User
+from app.models.history import History
 
 def init():
     print("nova 데이터베이스 생성중 ...")
-    Base.metadata.create_all(bind=engine) #새로 import 된 테이블만 생성
+    Base.metadata.create_all(bind=engine)
     print("nova 데이터베이스 생성 완료")
 
 if __name__ == "__main__":
