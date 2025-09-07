05d1b6646067cb15399ec88b1a3e27a0debdcb82
Yujung Kim <kimyj4017@gmail.com>
2025-09-07 16:00:14 +0900

    [refactor] .env 환경변수 기반으로 MongoDB 연결 강제 #24

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,6 +1,9 @@
 from typing import Optional
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 _MONGO_CLIENT: Optional[AsyncIOMotorClient] = None
 _DB = None
@@ -9,16 +12,20 @@ def init_mongo():
     global _MONGO_CLIENT, _DB
     if _MONGO_CLIENT:
         return
-    uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-    db_name = os.getenv("MONGO_DB", "nova")
+    
+    #env 파일 참고
+    uri = os.getenv("MONGO_URI")
+    db_name = os.getenv("MONGO_DB")
+
     _MONGO_CLIENT = AsyncIOMotorClient(uri)
     _DB = _MONGO_CLIENT[db_name]
 
 def get_mongo_db():
+    # 혹시 startup 전에 호출되면 지연 초기화
     if _DB is None:
-        # 혹시 startup 전에 호출되면 지연 초기화
         init_mongo()
     return _DB
 
 def get_results_coll():
-    return get_mongo_db()["video_results"]  # 컬렉션명 고정
+    # 컬렉션명 고정
+    return get_mongo_db()["video_results"]
